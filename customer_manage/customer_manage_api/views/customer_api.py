ad93c49ab4eec95ce880b42ef1682dc6168459aa
Tuan Anh <vtuananh1995@gmail.com>
2020-01-19 12:18:12 +0700

    create api interface and urls

---
+++ customer_manage/customer_manage_api/views/customer_api.py
@@ -0,0 +1,22 @@
+def create_customer():
+	pass
+
+
+def get_customer_ids():
+	pass
+
+
+def get_customer_infos():
+	pass
+
+
+def get_customer_details():
+	pass
+
+
+def update_customer():
+	pass
+
+
+def delete_customer():
+	pass
