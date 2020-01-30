4f35d689ec5f670bb6a8d7083e6776cbfb940250
vtaprime <vtuananh1995@gmail.com>
2020-01-30 21:00:28 +0700

    add script generate env variable

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export DB_NAME=customer_db
+export DB_USER=admin1
+export DB_PASS=12345678
+export DB_HOST=0.0.0.0
+export DB_PORT=5432
--- init_db.sh
+++ init_db.sh
@@ -4,12 +4,8 @@
 # First Bash Shell script to execute psql command
 ###################################################
 
-#Set the value of variable
-database="customer_db"
-
 #Execute few psql commands:
-#Note: you can also add -h hostname -U username in the below commands.
-psql -c "SELECT 'CREATE DATABASE customer_db' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'customer_db')\gexec"
-psql -d $database -c "CREATE TABLE public.customer_tab(id serial PRIMARY KEY,name character(255) NOT NULL,dob date NOT NULL,created_at bigint NOT NULL,updated_at bigint NOT NULL,is_deleted smallint NOT NULL,)"
-psql -d $database -c "INSERT INTO public.customer_tab (name, dob, created_at, updated_at, is_deleted) VALUES ('Anvesh', '1999-11-11', 1579534969, 1579534969, 0), ('Neevan', '1996-12-12', 1579535007, 1579535007, 0),('Roy', '1992-09-09', 1579535354, 1579535354, 0)"
-
+psql -h $DB_HOST -p $DB_PORT -d postgres -U $DB_USER -W -c "DROP DATABASE IF EXISTS customer_db"
+psql -h $DB_HOST -p $DB_PORT -d postgres -U $DB_USER -W -c "CREATE DATABASE customer_db"
+psql -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER -W -c "CREATE TABLE public.customer_tab(id serial PRIMARY KEY,name character(255) NOT NULL,dob date NOT NULL,created_at bigint NOT NULL,updated_at bigint NOT NULL,is_deleted smallint NOT NULL)"
+psql -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER -W -c "INSERT INTO public.customer_tab (name, dob, created_at, updated_at, is_deleted) VALUES ('Anvesh', '1999-11-11', 1579534969, 1579534969, 0), ('Neevan', '1996-12-12', 1579535007, 1579535007, 0),('Roy', '1992-09-09', 1579535354, 1579535354, 0)"
